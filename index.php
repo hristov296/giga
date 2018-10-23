c68c2caddd6ba772e44ba8dba862568e9f7dd209
hristov296 <hristov296@gmail.com>
2018-10-23 15:39:45 +0300

    repair

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php get_header($header) ?>
 <div id="primary">
   <div class="container">
-    <p>BAI HUI</p>
     <?php while (have_posts()) : the_post(); ?>
     <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
   	<?php the_content();
